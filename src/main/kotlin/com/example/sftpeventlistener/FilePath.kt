bf2a08e59f42051ccd14078a43612ede2180b593
lolmageap <ekxk1234@naver.com>
2024-02-03 18:41:42 +0900

    create file path

---
+++ src/main/kotlin/com/example/sftpeventlistener/FilePath.kt
@@ -0,0 +1,6 @@
+package com.example.sftpeventlistener
+
+object FilePath {
+    const val LOCAL_FILE_DIRECTORY = "/Users/cherhy/IdeaProjects/sftp-event-listener/src/main/resources/static/input"
+    const val ORIGIN_FILE_DIRECTORY = "/Users/cherhy/IdeaProjects/sftp-event-listener/src/main/resources/static/sftp"
+}
