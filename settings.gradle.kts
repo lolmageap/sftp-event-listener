313d7e3b9838c74c173cf0b68a27ef36b25cd85d
lolmageap <ekxk1234@naver.com>
2024-02-03 18:41:10 +0900

    create project

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "sftp-event-listener"
---
+++ src/test/kotlin/com/example/sftpeventlistener/SftpEventListenerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.sftpeventlistener
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SftpEventListenerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
