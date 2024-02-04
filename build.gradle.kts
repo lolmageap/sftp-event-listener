90ddbf8848d51653c2c4bdd987a4bb880e7fc2ef
lolmageap <ekxk1234@naver.com>
2024-02-04 16:54:54 +0900

    add log, jpa, h2 dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.4"
     kotlin("jvm") version "1.9.22"
     kotlin("plugin.spring") version "1.9.22"
+    kotlin("plugin.jpa") version "1.8.22"
 }
 
 group = "com.example"
@@ -22,7 +23,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("io.github.microutils:kotlin-logging:3.0.5")
     implementation("org.springframework.integration:spring-integration-sftp:6.2.1")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.h2database:h2")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
