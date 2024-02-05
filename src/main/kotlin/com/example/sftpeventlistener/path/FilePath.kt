ebbc8d8111bda174aa7c1c9847166b449b314f82
lolmageap <ekxk1234@naver.com>
2024-02-05 23:06:30 +0900

    fix origin file directory path

--- src/main/kotlin/com/example/sftpeventlistener/path/FilePath.kt
+++ src/main/kotlin/com/example/sftpeventlistener/path/FilePath.kt
@@ -2,7 +2,8 @@ package com.example.sftpeventlistener.path
 
 object FilePath {
     const val LOCAL_FILE_DIRECTORY = "/Users/cherhy/IdeaProjects/sftp-event-listener/src/main/resources/static/input"
-    const val ORIGIN_FILE_DIRECTORY = "/Users/cherhy/IdeaProjects/sftp-event-listener/src/main/resources/static/sftp"
+//    const val ORIGIN_FILE_DIRECTORY = "/Users/cherhy/IdeaProjects/sftp-event-listener2/src/main/resources/static/sftp" // 로컬 테스트 용
+    const val ORIGIN_FILE_DIRECTORY = "upload"
     const val SETTLEMENT_SUCCESS_FILE_DIRECTORY = "/Users/cherhy/IdeaProjects/sftp-event-listener/src/main/resources/static/settlement"
     const val ERROR_FILE_DIRECTORY = "/Users/cherhy/IdeaProjects/sftp-event-listener/src/main/resources/static/error"
 }
