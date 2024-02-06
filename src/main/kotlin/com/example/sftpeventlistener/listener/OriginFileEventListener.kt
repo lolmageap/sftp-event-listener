497ec7ee6cfcdb68a5a43152d7a7828f0d2bf01e
lolmageap <ekxk1234@naver.com>
2024-02-06 22:57:22 +0900

    fix DefaultSftpSessionFactory to CachingSessionFactory

--- src/main/kotlin/com/example/sftpeventlistener/listener/OriginFileEventListener.kt
+++ src/main/kotlin/com/example/sftpeventlistener/listener/OriginFileEventListener.kt
@@ -2,15 +2,15 @@ package com.example.sftpeventlistener.listener
 
 import com.example.sftpeventlistener.path.FilePath.LOCAL_FILE_DIRECTORY
 import com.example.sftpeventlistener.path.FilePath.ORIGIN_FILE_DIRECTORY
-import com.example.sftpeventlistener.service.FileService
+import org.apache.sshd.sftp.client.SftpClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.integration.channel.DirectChannel
 import org.springframework.integration.config.EnableIntegration
 import org.springframework.integration.dsl.IntegrationFlow
 import org.springframework.integration.dsl.Pollers
+import org.springframework.integration.file.remote.session.CachingSessionFactory
 import org.springframework.integration.sftp.dsl.Sftp
-import org.springframework.integration.sftp.session.DefaultSftpSessionFactory
 import org.springframework.messaging.MessageChannel
 import java.io.File
 
@@ -22,7 +22,8 @@ import java.io.File
 @Configuration
 @EnableIntegration
 class OriginFileEventListener(
-    private val sessionFactory: DefaultSftpSessionFactory,
+//    private val sessionFactory: DefaultSftpSessionFactory,
+    private val sessionFactory: CachingSessionFactory<SftpClient.DirEntry>,
 ) {
     @Bean
     fun fileInputChannel(): MessageChannel {
@@ -35,7 +36,9 @@ class OriginFileEventListener(
             Sftp.inboundAdapter(sessionFactory)
                 .remoteDirectory(ORIGIN_FILE_DIRECTORY)
                 .localDirectory(File(LOCAL_FILE_DIRECTORY))
-                .deleteRemoteFiles(true)
+                .deleteRemoteFiles(false)
+                .maxFetchSize(1)
+
         ) { p ->
             p.poller(
                 Pollers.fixedDelay(1000)
