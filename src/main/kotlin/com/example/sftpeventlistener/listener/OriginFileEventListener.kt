61f947f6fda73bdd2d47f435f78f0180fab8a5f1
lolmageap <ekxk1234@naver.com>
2024-02-15 23:07:14 +0900

    add meta data store and local filter

--- src/main/kotlin/com/example/sftpeventlistener/listener/OriginFileEventListener.kt
+++ src/main/kotlin/com/example/sftpeventlistener/listener/OriginFileEventListener.kt
@@ -1,6 +1,7 @@
 package com.example.sftpeventlistener.listener
 
 import com.example.sftpeventlistener.path.FilePath.LOCAL_FILE_DIRECTORY
+import com.example.sftpeventlistener.path.FilePath.LOCAL_FILE_META_DATA_STORE
 import com.example.sftpeventlistener.path.FilePath.ORIGIN_FILE_DIRECTORY
 import org.apache.sshd.sftp.client.SftpClient
 import org.springframework.context.annotation.Bean
@@ -9,7 +10,10 @@ import org.springframework.integration.channel.DirectChannel
 import org.springframework.integration.config.EnableIntegration
 import org.springframework.integration.dsl.IntegrationFlow
 import org.springframework.integration.dsl.Pollers
+import org.springframework.integration.file.filters.FileSystemPersistentAcceptOnceFileListFilter
 import org.springframework.integration.file.remote.session.CachingSessionFactory
+import org.springframework.integration.metadata.ConcurrentMetadataStore
+import org.springframework.integration.metadata.PropertiesPersistingMetadataStore
 import org.springframework.integration.sftp.dsl.Sftp
 import org.springframework.messaging.MessageChannel
 import java.io.File
@@ -18,11 +22,12 @@ import java.io.File
  *  sftp 에서 local 로 복사 해오는 클래스
  *  ex) 설정해 놓은 경로에 파일이 생성 되면 파일을 읽어서 처리 하는 설정
  *  handle 에서 파일을 읽어서 처리 하는 로직을 작성 하면 됨
+ *  metadataStore 는 파일을 읽었 는지 안읽었 는지 확인 하기 위한 저장소
+ *  localFilter 에서 읽은 데이터 는 중복 처리 하지 않기 위한 설정 ( 서버가 다운 되었 다가 다시 올라 오면 중복 처리 되는 것을 방지 하기 위한 설정)
  */
 @Configuration
 @EnableIntegration
 class OriginFileEventListener(
-//    private val sessionFactory: DefaultSftpSessionFactory,
     private val sessionFactory: CachingSessionFactory<SftpClient.DirEntry>,
 ) {
     @Bean
@@ -31,14 +36,25 @@ class OriginFileEventListener(
     }
 
     @Bean
+    fun metadataStore(): ConcurrentMetadataStore =
+        PropertiesPersistingMetadataStore().apply {
+            setBaseDirectory(LOCAL_FILE_META_DATA_STORE)
+            afterPropertiesSet()
+        }
+
+    @Bean
     fun fileReadingFlow(): IntegrationFlow {
         return IntegrationFlow.from(
             Sftp.inboundAdapter(sessionFactory)
                 .remoteDirectory(ORIGIN_FILE_DIRECTORY)
                 .localDirectory(File(LOCAL_FILE_DIRECTORY))
                 .deleteRemoteFiles(false)
-                .maxFetchSize(1)
-
+                .localFilter(
+                    FileSystemPersistentAcceptOnceFileListFilter(
+                        metadataStore(),
+                        "prefix",
+                    )
+                )
         ) { p ->
             p.poller(
                 Pollers.fixedDelay(1000)
