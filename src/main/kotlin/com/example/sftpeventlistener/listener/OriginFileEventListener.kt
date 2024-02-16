0f777d582e1a6b2198b41d3335aceeaff3c3744c
lolmageap <ekxk1234@naver.com>
2024-02-16 12:09:17 +0900

    add description and add SettlementUseCase

--- src/main/kotlin/com/example/sftpeventlistener/listener/OriginFileEventListener.kt
+++ src/main/kotlin/com/example/sftpeventlistener/listener/OriginFileEventListener.kt
@@ -3,6 +3,7 @@ package com.example.sftpeventlistener.listener
 import com.example.sftpeventlistener.path.FilePath.LOCAL_FILE_DIRECTORY
 import com.example.sftpeventlistener.path.FilePath.LOCAL_FILE_META_DATA_STORE
 import com.example.sftpeventlistener.path.FilePath.ORIGIN_FILE_DIRECTORY
+import com.example.sftpeventlistener.usecase.SettlementUseCase
 import org.apache.sshd.sftp.client.SftpClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -19,22 +20,26 @@ import org.springframework.messaging.MessageChannel
 import java.io.File
 
 /**
- *  sftp 에서 local 로 복사 해오는 클래스
- *  ex) 설정해 놓은 경로에 파일이 생성 되면 파일을 읽어서 처리 하는 설정
- *  handle 에서 파일을 읽어서 처리 하는 로직을 작성 하면 됨
- *  metadataStore 는 파일을 읽었 는지 안읽었 는지 확인 하기 위한 저장소
- *  localFilter 에서 읽은 데이터 는 중복 처리 하지 않기 위한 설정 ( 서버가 다운 되었 다가 다시 올라 오면 중복 처리 되는 것을 방지 하기 위한 설정)
+ *  <h3> sftp 에서 local 로 복사 해오는 클래스 </h3>
+ *  <p> ex) 설정해 놓은 경로에 파일이 생성 되면 파일을 읽어서 처리 하는 설정 </p>
  */
+
 @Configuration
 @EnableIntegration
 class OriginFileEventListener(
     private val sessionFactory: CachingSessionFactory<SftpClient.DirEntry>,
+    private val settlementUseCase: SettlementUseCase,
 ) {
     @Bean
     fun fileInputChannel(): MessageChannel {
         return DirectChannel()
     }
 
+    /**
+     *  <p> metadataStore 는 파일을 읽었 는지 안읽었 는지 확인 하기 위한 저장소 </p>
+     *  <p> localFilter 에서 읽은 데이터 는 중복 처리 하지 않기 위한 설정 </p>
+     *  <p> ( 서버가 다운 되었 다가 다시 올라 오면 중복 처리 되는 것을 방지 하기 위한 설정) </p>
+     */
     @Bean
     fun metadataStore(): ConcurrentMetadataStore =
         PropertiesPersistingMetadataStore().apply {
@@ -42,6 +47,13 @@ class OriginFileEventListener(
             afterPropertiesSet()
         }
 
+    /**
+     * <p> sftp 서버에서 파일을 읽어서 local 로 복사 하는 설정 </p>
+     * <p> poller 를 사용 하여 주기적으로 파일을 읽어서 처리 하도록 설정 </p>
+     * <p> fileInputChannel 에서 파일을 읽어서 처리 하도록 설정 </p>
+     * <p> handle 에서 파일을 읽어서 처리 하는 로직을 구현 </p>
+     * <p> 파일을 읽어서 처리 하는 로직은 settlementUseCase 에서 구현 </p>
+     */
     @Bean
     fun fileReadingFlow(): IntegrationFlow {
         return IntegrationFlow.from(
@@ -62,7 +74,7 @@ class OriginFileEventListener(
         }.channel(
             fileInputChannel()
         ).handle<File> { file, _ ->
-            println("file = $file")
+            settlementUseCase.settlement(file)
         }
         .get()
     }
