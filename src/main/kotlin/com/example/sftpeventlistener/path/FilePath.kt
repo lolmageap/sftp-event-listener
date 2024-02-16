735d310e4df527992c386f580684105c175a4fab
lolmageap <ekxk1234@naver.com>
2024-02-16 12:29:48 +0900

    separate ORIGIN_FILE_DIRECTORY

--- src/main/kotlin/com/example/sftpeventlistener/path/FilePath.kt
+++ src/main/kotlin/com/example/sftpeventlistener/path/FilePath.kt
@@ -3,6 +3,7 @@ package com.example.sftpeventlistener.path
 object FilePath {
     const val LOCAL_FILE_DIRECTORY = "/Users/cherhy/IdeaProjects/sftp-event-listener/src/main/resources/static/input"
     const val LOCAL_FILE_META_DATA_STORE = "/Users/cherhy/IdeaProjects/sftp-event-listener/src/main/resources/static/input/.metadata"
-    const val ORIGIN_FILE_DIRECTORY = "/upload"
+    const val ORIGIN_PULL_FILE_DIRECTORY = "/pull"
+    const val ORIGIN_PUSH_FILE_DIRECTORY = "/push"
     const val ERROR_FILE_DIRECTORY = "/Users/cherhy/IdeaProjects/sftp-event-listener/src/main/resources/static/error"
 }
