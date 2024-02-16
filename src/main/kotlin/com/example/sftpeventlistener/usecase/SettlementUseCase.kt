2cfd522ebddc49e184560075340eba7d853afeb1
lolmageap <ekxk1234@naver.com>
2024-02-16 12:09:51 +0900

    remove copy and delete logic

--- src/main/kotlin/com/example/sftpeventlistener/usecase/SettlementUseCase.kt
+++ src/main/kotlin/com/example/sftpeventlistener/usecase/SettlementUseCase.kt
@@ -17,21 +17,13 @@ class SettlementUseCase(
     private val settlementCalculator: SettlementCalculator,
 ) {
 
-    /**
-     * finally 블록을 사용 하여 파일을 삭제 하게 되면 errorHandling 실패 시에도 파일이 삭제 됨
-     */
     fun settlement(file: File) {
         try {
             val amount = settlementCalculator.calculateSettlement(file)
             settlementService.save(file.name, amount)
-
-            fileService.copyToSettlementSuccess(file)
-            fileService.deleteLocalFile(file)
-
         } catch (e: SettlementFailException) {
             logger.error { "정산 처리 실패: ${e.message}" }
             fileService.copyToError(file)
-            fileService.deleteLocalFile(file)
         }
     }
 
