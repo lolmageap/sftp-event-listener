7f016e86700e705f422cff81b711861d8f3c78fe
lolmageap <ekxk1234@naver.com>
2024-02-16 12:07:54 +0900

    remove success directory

--- src/main/kotlin/com/example/sftpeventlistener/path/FilePath.kt
+++ src/main/kotlin/com/example/sftpeventlistener/path/FilePath.kt
@@ -3,8 +3,6 @@ package com.example.sftpeventlistener.path
 object FilePath {
     const val LOCAL_FILE_DIRECTORY = "/Users/cherhy/IdeaProjects/sftp-event-listener/src/main/resources/static/input"
     const val LOCAL_FILE_META_DATA_STORE = "/Users/cherhy/IdeaProjects/sftp-event-listener/src/main/resources/static/input/.metadata"
-//    const val ORIGIN_FILE_DIRECTORY = "/Users/cherhy/IdeaProjects/sftp-event-listener2/src/main/resources/static/sftp" // 로컬 테스트 용
-    const val ORIGIN_FILE_DIRECTORY = "upload"
-    const val SETTLEMENT_SUCCESS_FILE_DIRECTORY = "/Users/cherhy/IdeaProjects/sftp-event-listener/src/main/resources/static/settlement"
+    const val ORIGIN_FILE_DIRECTORY = "/upload"
     const val ERROR_FILE_DIRECTORY = "/Users/cherhy/IdeaProjects/sftp-event-listener/src/main/resources/static/error"
 }
