3a97dd55c07508f971957a64bc0e3fc0eb47bb9e
lolmageap <ekxk1234@naver.com>
2024-02-24 18:45:42 +0900

    fix metadata store path

--- src/main/kotlin/com/example/sftpeventlistener/path/FilePath.kt
+++ src/main/kotlin/com/example/sftpeventlistener/path/FilePath.kt
@@ -2,7 +2,7 @@ package com.example.sftpeventlistener.path
 
 object FilePath {
     const val LOCAL_FILE_DIRECTORY = "/Users/cherhy/IdeaProjects/sftp-event-listener/src/main/resources/static/input"
-    const val LOCAL_FILE_META_DATA_STORE = "/Users/cherhy/IdeaProjects/sftp-event-listener/src/main/resources/static/input/.metadata"
+    const val LOCAL_FILE_META_DATA_STORE = "/Users/cherhy/IdeaProjects/sftp-event-listener/src/main/resources/static/.metadata"
     const val ORIGIN_PULL_FILE_DIRECTORY = "/pull"
     const val ORIGIN_PUSH_FILE_DIRECTORY = "/push"
     const val ERROR_FILE_DIRECTORY = "/Users/cherhy/IdeaProjects/sftp-event-listener/src/main/resources/static/error"
